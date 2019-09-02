3e6bd42fb0116a465ebb9d75e7e12c5cc502ec8b
Patryk Szalanski <patryk.szalanski@linkmobility.com>
2019-09-02 09:58:38 +0200

    Moved config to class

--- LinkSmsRestApi(Send)/Samples/python/send_sms.py
+++ LinkSmsRestApi(Send)/Samples/python/send_sms.py
@@ -1,66 +1,70 @@
 #!env python3
 
 from argparse import ArgumentParser
+from collections import namedtuple
 
 from requests import post
 from requests.auth import HTTPBasicAuth
 
-server = None
-user = None
-password = None
 
+class SmsSender(object):
 
-def do_request(path, data):
-    url = f'{server}{path}'
-    print('sending to ', url)
-    print(post(url, json=data, auth=HTTPBasicAuth(user, password)))
+    def __init__(self, server, user, password, msisdns):
+        self.server = server
+        self.auth = HTTPBasicAuth(user, password)
+        self.msisdns = msisdns
 
+    def do_request(self, path, data):
+        response = post(self.server + path, json=data, auth=self.auth)
+        print(response)
 
-def send_single_sms(msisdns):
-    do_request('/sms/send', {
-        "source": "LINK",
-        "destination": msisdns[0],
-        "userData": "Hello world",
-        "platformId": "0",
-        "platformPartnerId": "0",
-        "useDeliveryReport": False})
+    def send_single_sms(self):
+        self.do_request('/sms/send', {
+            "source": "LINK",
+            "destination": self.msisdns[0],
+            "userData": "Hello world",
+            "platformId": "0",
+            "platformPartnerId": "0",
+            "useDeliveryReport": False})
 
+    def send_bulk_sms(self):
+        pass
 
-def send_bulk_sms(msisdns):
-    pass
+    def send_payment_sms(self):
+        pass
 
-
-def send_payment_sms(msisdns):
-    pass
-
-
-commands = {
-    'send': send_single_sms,
-    'send_bulk': send_bulk_sms,
-    'send_payment': send_payment_sms
-}
+    def execute(self, command):
+        commands = {
+        'send': self.send_single_sms,
+        'send_bulk': self.send_bulk_sms,
+        'send_payment': self.send_payment_sms
+        }
+        commands[command]()
 
 
 def main():
     parser = ArgumentParser()
 
-    parser.add_argument('command', choices=commands.keys(), metavar='COMMAND',
+    parser.add_argument('command',
+                        choices=['send', 'send_bulk', 'send_payment'],
+                        metavar='COMMAND',
                         help='Available commands: %(choices)s')
-    parser.add_argument('msisdns', nargs='+', metavar='MSISDNS',
+    parser.add_argument('msisdns',
+                        nargs='+',
+                        metavar='MSISDNS',
                         help='One or more MSISDNs to test sending with')
 
     args = parser.parse_args()
 
+
     try:
         with open('./server_settings.txt') as server_settings:
-            global server
-            global user
-            global password
-            server, user, password = [l.strip() for l in server_settings.readlines()]
+            config = [l.strip() for l in server_settings.readlines()]
     except FileNotFoundError:
         print('File server_settings.txt not found!')
 
-    commands[args.command](args.msisdns)
+    sms_sender = SmsSender(config[0], config[1], config[2], args.msisdns)
+    sms_sender.execute(args.command)
 
 
 if __name__ == '__main__':
