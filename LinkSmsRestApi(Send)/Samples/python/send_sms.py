27acded0151f15d0d5a0e9864e9c52a2109e07c7
Patryk Szalanski <patryk.szalanski@linkmobility.com>
2019-09-02 13:26:02 +0200

    Switch configuration to json

--- LinkSmsRestApi(Send)/Samples/python/send_sms.py
+++ LinkSmsRestApi(Send)/Samples/python/send_sms.py
@@ -1,30 +1,42 @@
 #!env python3
 
+import json
+import os
 from argparse import ArgumentParser
 from collections import namedtuple
+from pathlib import Path
 
 from requests import post
 from requests.auth import HTTPBasicAuth
 
+basedir = Path(os.path.dirname(os.path.realpath(__file__)))
+
 
 class SmsSender(object):
 
-    def __init__(self, server, user, password, msisdns):
-        self.server = server
-        self.auth = HTTPBasicAuth(user, password)
+    def __init__(self, dry_run, config, msisdns):
+        self.dry_run = dry_run
+        self.server_url = config['server_url']
+        self.platformId = config['platformId']
+        self.platformPartnerId = config['platformPartnerId']
+        self.auth = HTTPBasicAuth(config['username'], config['password'])
         self.msisdns = msisdns
 
     def do_request(self, path, data):
-        response = post(self.server + path, json=data, auth=self.auth)
-        print(response)
+        if not self.dry_run:
+            response = post(self.server_url + path, json=data, auth=self.auth)
+            print(response.json())
+        else:
+            print('### Dry run (override to do real request using -n)')
+            print(f'Request would be: POST {self.server_url + path}\nBody: {data}')
 
     def send_single_sms(self):
-        self.do_request('/sms/send', {
+        self.do_request('/send', {
             "source": "LINK",
             "destination": self.msisdns[0],
-            "userData": "Hello world",
-            "platformId": "0",
-            "platformPartnerId": "0",
+            "userData": "Hello, this is a single test message.",
+            "platformId": self.platformId,
+            "platformPartnerId": self.platformPartnerId,
             "useDeliveryReport": False})
 
     def send_bulk_sms(self):
@@ -53,17 +65,18 @@ def main():
                         nargs='+',
                         metavar='MSISDNS',
                         help='One or more MSISDNs to test sending with')
+    parser.add_argument('-n', '--no-dry-run', action='store_false', dest='dry_run')
 
     args = parser.parse_args()
 
 
     try:
-        with open('./server_settings.txt') as server_settings:
-            config = [l.strip() for l in server_settings.readlines()]
+        with open(basedir / 'server_settings.json') as server_settings:
+            config = json.load(server_settings)
     except FileNotFoundError:
         print('File server_settings.txt not found!')
 
-    sms_sender = SmsSender(config[0], config[1], config[2], args.msisdns)
+    sms_sender = SmsSender(args.dry_run, config, args.msisdns)
     sms_sender.execute(args.command)
 
 
